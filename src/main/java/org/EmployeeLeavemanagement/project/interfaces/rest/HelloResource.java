6e46a081311942eaa585ba403fb0e0104fb51252
Pooja16596 <poojatiwari16596@gmail.com>
2020-11-09 06:00:35 -0800

    first commit

---
+++ src/main/java/org/EmployeeLeavemanagement/project/interfaces/rest/HelloResource.java
@@ -0,0 +1,12 @@
+package org.EmployeeLeavemanagement.project.interfaces.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("hello")
+public class HelloResource {
+    @GET
+    public String hello() {
+        return "Hello World!";
+    }
+}
