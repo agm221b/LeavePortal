34ad26a2d7a682210baa01e3a1a342c23b2a002a
Harish <harish.maurya13@gmail.com>
2020-11-13 11:33:54 +0530

    SeedStack removed Spring Boot Added

---
+++ LeaveManagementSys/src/main/java/com/yp/lms/LeaveManagementSysApplication.java
@@ -0,0 +1,14 @@
+package com.yp.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveManagementSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveManagementSysApplication.class, args);
+		System.out.println("Started");
+	}
+
+}
