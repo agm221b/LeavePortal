ca6970765d045f222034f961b3205a0d3d3feceb
Harish <harish.maurya13@gmail.com>
2020-11-22 19:48:54 +0530

    In pom.xml spring security core dependency added for password encryption. @bean obj created in main class

--- LeaveManagementSys/src/main/java/com/yp/lms/LeaveManagementSysApplication.java
+++ LeaveManagementSys/src/main/java/com/yp/lms/LeaveManagementSysApplication.java
@@ -2,6 +2,8 @@ package com.yp.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class LeaveManagementSysApplication {
@@ -9,6 +11,11 @@ public class LeaveManagementSysApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LeaveManagementSysApplication.class, args);
 		System.out.println("Started");
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
